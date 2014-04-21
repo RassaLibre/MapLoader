779fbae9ad66c3814b6e5c61560bcee3428d473e
Tomas Prochazka <tomasprochazka@Tomass-MacBook-Pro.local>
2014-04-21 20:09:00 +0200

    pics

--- server.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname));
-app.listen(3000); //the port you want to use
-console.log('The server is listening on http://localhost:3000/');
